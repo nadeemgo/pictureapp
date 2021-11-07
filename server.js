8f92a2efe27e932c1c184a236ecbb0075015bf36
nadeemgo <nadeemgoharweb@gmail.com>
2021-11-07 18:07:45 -0500

    layout ejs home ejs  commit

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.use((req, res, next) => {
     next()
 })
 
-app.use(express.static('/public'))
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
